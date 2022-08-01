e9eaefb074f2b47997173462c606bd1e380b269e
Proladon <Proladon@gmail.com>
2022-08-01 16:36:50 +0800

    add: form api

---
+++ src/api/form.ts
@@ -0,0 +1,47 @@
+import { api, AxiosInstanceResponse } from './instance'
+
+export const GetFormList = async ({
+  last,
+  limit,
+}: {
+  last?: string
+  limit?: number
+}) => {
+  return await api({
+    method: 'GET',
+    url: `/forms`,
+    params: {
+      last,
+      limit,
+    },
+  })
+}
+
+export const FindForm = async ({ formKey }: { formKey: string }) => {
+  return await api({
+    method: 'GET',
+    url: `/forms/${formKey}`,
+  })
+}
+
+export const SubmitForm = async ({
+  formKey,
+  content,
+  imgs,
+  userId,
+}: {
+  formKey: string
+  content: string
+  imgs: string[]
+  userId?: string
+}) => {
+  return await api({
+    method: 'POST',
+    url: `/forms/${formKey}`,
+    data: {
+      content,
+      imgs,
+      userId,
+    },
+  })
+}
---
+++ src/api/instance.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosResponse, AxiosError } from 'axios'
+
+export type AxiosInstanceResponse = Promise<[any | null, Error | null]>
+
+export const api = axios.create({
+  baseURL: 'https://sz-form.deta.dev',
+})
+
+// Handler
+const handleSuccessRes = (response: AxiosResponse) => {
+  return [response.data, null]
+}
+const handleErrorRes = (error: AxiosError) => {
+  return [null, error.response || error]
+}
+
+// Res
+api.interceptors.response.use(handleSuccessRes, handleErrorRes)
+// Req
+api.interceptors.request.use(async (config: any) => {
+  // config.headers.Authorization = `Bot ${process.env.BOT_TOKEN}`
+  return config
+})
