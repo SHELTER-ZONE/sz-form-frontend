c1fe1ef37f4c8425144b672d2bd7bc71d7ad6e54
Proladon <Proladon@gmail.com>
2022-08-01 15:38:35 +0800

    fix: layout struct

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <Provider>
-    <router-view />
+  <Provider class="flex flex-col">
+    <Navbar />
+    <router-view class="flex-1 common-width !mt-[50px]" />
+    <SzFooter />
   </Provider>
 </template>
 
 <script setup lang="ts">
 import Provider from '@/components/Provider.vue'
+import SzFooter from '@/components/SzFooter.vue'
 </script>
 
 <style lang="postcss">
