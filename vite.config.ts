bd80bb349946e1463482621f4e913589cda22180
Proladon <Proladon@gmail.com>
2022-08-06 14:28:41 +0800

    fix: baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { resolve, join } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), WindiCSS()],
-  base: '/SHELTER-ZONE/sz-form-frontend/',
+  base: '/sz-form-frontend/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
